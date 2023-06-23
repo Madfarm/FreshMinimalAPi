43fcfaa7479beb8e208932d8ff546a880ce4a8c6
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-23 15:20:01 -0400

    Add some global using statements

---
+++ Helpers/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using FruitStand.Data;
+
+global using MongoDB.Bson;
+global using MongoDB.Bson.Serialization.Attributes;
+global using MongoDB.Driver;
+global using Microsoft.Extensions.Options;
+global using Microsoft.OpenApi.Models;
